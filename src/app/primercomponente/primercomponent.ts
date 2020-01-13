6e9d5cf732b9417e1eae588575e120a4de5afef7
Emi <you@example.com>
2020-01-13 00:49:54 -0300

    continue implementing angular test

--- src/app/primercomponente/primercomponent.ts
+++ src/app/primercomponente/primercomponent.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'hello-world',
-    template: `
-    <div class="hello-world">
-        <h1>Mi novia</h1>
-        <h2>Me ama y me apreta las bolas</h2>
-    </div>
-    `,
-    styles: ['.hello-world {background: #000; color: #fff;}']
+    templateUrl:'./primercomponent.html',
+    styles: ['./primercomponent.css']
 })
 export class HelloWorld{
-
+    title = "Welcome to angular";
 }
